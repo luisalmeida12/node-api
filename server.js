751daff508dfb299fe8e4857939b1f232de39d49
Luis Almeida <info@luis-almeida.com>
2019-08-06 22:29:11 +0100

    Restructuring project.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+
 // Iniciando o App
 const app = express();
 
@@ -11,18 +12,7 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-
-const Product = mongoose.model('Product');
-
-/// Primeira Rota
-app.get('/',(req,res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    });
-    
-    return res.send('Hello Wosssrld');
-});
+// Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
