66898d4ede6c905749e55dc6a59f076267d4c6cc
Luis Almeida <info@luis-almeida.com>
2019-08-06 22:50:40 +0100

    Creating a product.

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,5 +6,12 @@ module.exports = {
     async index(req, res) {
         const products = await Product.find();
         return res.json(products);
+    },
+
+    async store(req, res) {
+        // criacao
+        const product = await Product.create(req.body);
+
+        return res.json(product);
     }
 }
